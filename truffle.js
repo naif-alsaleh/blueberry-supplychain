12f5df4271c9f27440bb84dabd4088d8e8b2dc5f
jal <jaldemad@gmail.com>
2019-03-10 18:20:43 +0300

    add rinkeby configuration

--- truffle.js
+++ truffle.js
@@ -1,9 +1,19 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: function () {
+        return new HDWalletProvider("ivory object galaxy garbage fiction level cruise swim pluck domain collect load", "https://rinkeby.infura.io/v3/c0c2ccd635d44b6cb9da57c72299cabc")
+      },
+      network_id: '4',
+      gas: 4500000,
+      gasPrice: 10000000000,
     }
   },
   compilers: {
